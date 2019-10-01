8ebfd6d4b402d9895f229a16c73a15952cd148c1
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2019-10-01 15:54:05 +0200

    update

--- sketch.js
+++ sketch.js
@@ -18,10 +18,29 @@ function draw() {
 
   var x = 0;
   var y = 50;
-  var colorList = ['	#fff200',
-                 '#ffb700',
+  var colorList = ['	#000000',
+                 '#ffffff',
+
                  '	#9533ff',
-                 '	#3733ff'
+                 '	#3733ff',
+
+                 '	#4cff3d',
+                 '	#79b521',
+
+                 '	#ed1c24',
+                 '	#b44b24',
+
+                 '	#bcbec0',
+                 '	#6d6e71',
+
+                 '	#4cf070',
+                 '	#00a79d',
+
+                 '	#ff0a65',
+                 '	#b531b0',
+
+                 '	#fff200',
+                 '	#fbb040',
                               ];
 
 
@@ -36,10 +55,55 @@ function draw() {
     var y4 = y+50;
     noStroke();
 
-    if (random()<0.5)
+    if (random()<=0.5 && pmouseX<250)
     { fill(colorList[2]); }
 
-    else {  fill(colorList[3]);  }
+    else if (random()>=0.5 && pmouseX<250)
+    { fill(colorList[3]); }
+
+
+    else if (random()<=0.5 && pmouseX<450 && pmouseX>250)
+    { fill(colorList[4]); }
+
+    else if (random()>=0.5 && pmouseX<450 && pmouseX>250)
+    { fill(colorList[5]); }
+
+
+    else if (random()<=0.5 && pmouseX<650 && pmouseX>450)
+    { fill(colorList[6]); }
+
+    else if (random()>=0.5 && pmouseX<650 && pmouseX>450)
+    { fill(colorList[7]); }
+
+
+    else if (random()<=0.5 && pmouseX<850 && pmouseX>650)
+    { fill(colorList[8]); }
+
+    else if (random()>=0.5 && pmouseX<850 && pmouseX>650)
+    { fill(colorList[9]); }
+
+
+    else if (random()<=0.5 && pmouseX<1050 && pmouseX>850)
+    { fill(colorList[10]); }
+
+    else if (random()>=0.5 && pmouseX<1050 && pmouseX>850)
+    { fill(colorList[11]); }
+
+
+    else if (random()<=0.5 && pmouseX<1250 && pmouseX>1050)
+    { fill(colorList[12]); }
+
+    else if (random()>=0.5 && pmouseX<1250 && pmouseX>1050)
+    { fill(colorList[13]); }
+
+
+    else if (random()<=0.5 && pmouseX<1450 && pmouseX>1250)
+    { fill(colorList[14]); }
+
+    else if (random()>=0.5 && pmouseX<1450 && pmouseX>1250)
+    { fill(colorList[15]); }
+
+    else { noFill(); }
 
   quad(x, y, x2, y2, x3, y3, x4, y4) ;
   }
